384a74eb994497c5f0f4d0011ce98ad34914d5ff
Rishi Ajoykumar <rishiajoykumar@MacBook-Pro-10.local>
2024-09-14 12:05:02 -0500

    created mock functions in ApiService and added filter buttons to AppComponent

---
+++ src/services/api-service.js
@@ -0,0 +1,15 @@
+export function fetchTasks () {
+	console.log("fetch tasks");
+}
+
+export function addTask () {
+	console.log("add task");
+}
+
+export function deleteTask (id) {
+	console.log("delete task");
+}
+
+export function changeActiveStatus (id) {
+	console.log("change active status");
+}
